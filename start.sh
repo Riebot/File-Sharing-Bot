8679fbbdfe0bb77e617e37f494dd7d4fa196397b
Zaute <77600757+ZauteKm@users.noreply.github.com>
2021-09-12 12:50:24 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,6 +2,6 @@ echo "[INFO] - FETCHING UPSTREAM REPO ..."
 git clone https://github.com/ZauteKm/File-Sharing-Bot.git /File-Sharing-Bot
 cd /File-Sharing-Bot
 echo "[INFO] - INSTALLING REQUIREMENTS ..."
-pip3 install -U -r requirements.txt
+pip3 install -r requirements.txt
 echo "[INFO] - STARTING FILE-SHARING-BOT ..."
 python3 main.py
