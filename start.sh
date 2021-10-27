4b26778277d45d4ea38b7c5d0d974b08e5c35612
Zaute Km <77600757+ZauteKm@users.noreply.github.com>
2021-10-27 15:01:26 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 echo "Cloning Repo...."
 git clone https://github.com/ZauteKm/File-Sharing-Bot.git /File-Sharing-Bot
 cd /File-Sharing-Bot
-pip3 install -r requirements.txt
+pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 main.py
