05c871473cff624e11707043823e57f76ac78303
Zaute Km <77600757+ZauteKm@users.noreply.github.com>
2021-10-27 16:15:41 +0530

    Update config.py

--- config.py
+++ config.py
@@ -26,7 +26,7 @@ FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "0"))
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 
 #start message
-START_MSG = os.environ.get("START_MESSAGE", "<b>Hi {mention},\n\nKei hi ZoSeriesBot ka ni e.</b>\nA hnuaia Group & Channel te hi Join Rawh :)\n\n<b>Send /help for more details.</b>.")
+START_MSG = os.environ.get("START_MESSAGE", "<b>Hi {mention},\n\nKei hi ZoSeriesBot ka ni e.</b>\nA hnuaia Group & Channel te hi Join Rawh :)\n\n<b>Send /help for more details</b>.")
 try:
     ADMINS=[]
     for x in (os.environ.get("ADMINS", "").split()):
@@ -35,7 +35,7 @@ except ValueError:
         raise Exception("Your Admins list does not contain valid integers.")
 
 #Force sub message 
-FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "Hello {first}\n\n<b>Khawngaihin @mizotginfotel Channel hi Join phawt la, Chuan min hmang thei ang.</b>")
+FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "Hello {first}\n\n<b>Khawngaihin @mizotginfotel Channel hi Join phawt la, Chuan min hmang thei ang</b>.")
 
 #set your Custom Caption here, Keep None for Disable Custom Caption
 CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", "<b>File Name:</b> <code>{filename}</code>\n\n<b>Join <a href='https://t.me/joinchat/prE6ALN6x2hkY2E1'>ZoSeries Studio</a> for more useful bots</b>\n\n<b>Others:</b> {previouscaption}")
