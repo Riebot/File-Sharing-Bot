3d98fa217999e09c01ddc94aa65ca7967ab733a9
Zaute Km <77600757+ZauteKm@users.noreply.github.com>
2021-10-31 12:02:29 +0530

    Create cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>▷ Creator : <a href='tg://user?id={OWNER_ID}'>This Person</a>\n▷ Language : <code>Python3</code>\n▷ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n▷ Movies/Series : <a href='https://t.me/joinchat/prE6ALN6x2hkY2E1'>Join now</a>\n▷ Channel : <a href='https://t.me/mizotginfotel'>Mizo Infotel Channel</a>\n▷ Support Group : <a href='https://t.me/mizotelegram'>Join now</a></b>",
+            text = f"<b>▷ Creator: <a href='tg://user?id={OWNER_ID}'>This Person</a>\n▷ Language: <code>Python3</code>\n▷ Library: <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n▷ Movies/Series: <a href='https://t.me/joinchat/prE6ALN6x2hkY2E1'>Join now</a>\n▷ Channel: <a href='https://t.me/mizotginfotel'>Mizo Infotel Channel</a>\n▷ Support Group: <a href='https://t.me/mizotelegram'>Join now</a></b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
@@ -37,7 +37,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "help":
         await query.message.edit_text(
-            text = f"<b>Min hmandân túr tlângpui:</b>\n\n1.1. Inline Mode hmangin i Series/Movies duh i Search thei.\n2. He <a href='https://t.me/joinchat/prE6ALN6x2hkY2E1'>Group</a> hi Join-in i Series duh hming thupui lo thawn tawp la, i hmu nghâl ang.\n\n<b>How to Get Files:</b> <a href='https://t.me/mizotginfotel/37'>Press me 🥰</a>",
+            text = f"<b>Min hmandân túr tlângpui:</b>\n\n1. Inline Mode hmangin i Series/Movies duh i Search thei.\n2. He <a href='https://t.me/joinchat/prE6ALN6x2hkY2E1'>Group</a> hi Join-in i Series duh hming thupui lo thawn tawp la, i hmu nghâl ang.\n\n<b>How to Get Files:</b> <a href='https://t.me/mizotginfotel/37'>Press me 🥰</a>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
